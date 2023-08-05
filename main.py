2da9f3219e66f5ae56c69e920cb3d70228740ff9
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-05 12:58:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,24 +10,21 @@ from pandas.api.types import is_numeric_dtype
 import random
 import csv
 
-#functions
+#funzioni
 def estraiGiocatore(df):
     giocatore = df.sample(n=1)
     st.dataframe(giocatore)
+    st.write(df[(df["cognome"] == giocatore["cognome"]) & (df["squadra"] == giocatore["squadra"])])
+    df.drop(, axis = 0, inplace = True)
     
 
 #importazione lista calciatori
 table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/prog_fanta/main/fanta/ListaGiocatori.CSV', delimiter = ";")
 
-#Titolo apllicazione
+#Apllicazione
 st.title("Applicazione Fanta")
 
-left, center, right = st.columns(3, gap = "large")
-with center:
-    colB1, colB2, colB3 = st.columns([0.15, 0.7, 0.15], gap = "large")
-    with colB2:
-        if st.button('Estrai Giocatore'): estraiGiocatore(table)
-    
+if st.button('Estrai Giocatore'): estraiGiocatore(table)
 
 left, center, right = st.columns([0.4, 0.4, 0.2], gap = "large")
 with left: 
