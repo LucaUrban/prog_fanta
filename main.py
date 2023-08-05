e2cd062a6c395af2b98d85dffaa996c799ed8b7e
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-05 18:43:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def estraiGiocatore(df):
 def inserisciGiocatoreDataframe(df, giocatore, price):
     idx = min(df[df["Prezzo"] == 0].index)
     df.loc["Cognome", idx] = "abc"
-    st.write(giocatore["Cognome"].values)
+    st.write(giocatore)
     df.loc["Prezzo", idx] = price
     #st.write(df["Ruolo"][idx])
 
