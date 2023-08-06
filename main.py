922285db168aca2f0991b8da2766d3b96786ca03
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-06 13:46:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,7 @@ def createExcel():
 @st.cache_resource
 def init_connection():
     return pymongo.MongoClient(**st.secrets["mongo"])
+st.write("jsnvjfdb")
 st.write(**st.secrets["mongo"])
 client = init_connection()
 
