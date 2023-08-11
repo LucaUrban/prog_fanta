928d5807298599561bdf15045bda63c0cb86beeb
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-11 09:14:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ client = init_connection()
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def get_data():
-    db = client.mydb
-    items = db.mycollection.find()
+    db = client["Fantacalcio"]
+    items = db["Squadre"].find()
     items = list(items)  # make hashable for st.cache_data
     return items
 
