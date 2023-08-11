6e0a4fbac9096a0c1da4844dde5426c7ffa469f0
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-11 14:12:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import csv
 import pymongo
 
 #funzione estrazione casuale calciatore
-def inserisciGiocatoreDataframe(id, cognome, price):
+def inserisciGiocatoreDataframe(df, cognome, price):
     idx = min(df[df["Prezzo"] == 0].index)
     df.loc[idx, "Cognome"] = cognome
     st.write(cognome)
@@ -37,6 +37,16 @@ def get_data():
     return items
 
 data = get_data()
+
+#costruzione dataframe giocatori
+Alessandro = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Andrea = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Federico = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Gabriele = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Luca = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Michele = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Valerio = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
+Valter = pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"])
 
 #importazione lista calciatori
 table = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/prog_fanta/main/fanta/ListaGiocatori.CSV', delimiter = ";")
@@ -74,26 +84,26 @@ with right:
 col1, col2, col3, col4 = st.columns(4, gap = "small")
 with col1:
     st.write("Alessandro")
-    st.dataframe(pd.DataFrame(data[0]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.dataframe(Alessandro, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.write("Michele")
-    st.dataframe(pd.DataFrame(data[4]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.write("Luca")
+    st.dataframe(Luca, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
 with col2:
     st.write("Andrea")
-    st.dataframe(pd.DataFrame(data[1]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.dataframe(Andrea, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.write("Morto")
-    st.dataframe(pd.DataFrame(data[5]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.write("Michele")
+    st.dataframe(Michele, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
 with col3:
-    st.write("Gabriele")
-    st.dataframe(pd.DataFrame(data[2]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.write("Federico")
+    st.dataframe(Federico, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
     st.write("Valerio")
-    st.dataframe(pd.DataFrame(data[6]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.dataframe(Valerio, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
 with col4:
-    st.write("Luca")
-    st.dataframe(pd.DataFrame(data[3]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.write("Gabriele")
+    st.dataframe(Gabriele, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
     st.write("Valter")
-    st.dataframe(pd.DataFrame(data[7]['Squadra'], columns = ["Ruolo", "Cognome", "Prezzo"]), use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
+    st.dataframe(Valter, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
 
