221d07f5ae9409e0a386db0028faa67e08b86b07
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-14 11:24:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ Valter = pd.DataFrame(next(item for item in data if item["Partecipante"] == "Val
 @st.cache_data(ttl = 600)
 def readTableGiocatori():
     return pd.read_csv('https://raw.githubusercontent.com/LucaUrban/prog_fanta/main/fanta/ListaGiocatori.CSV', delimiter = ";").values.tolist()
-table = readTableGiocatori()
+table = pd.DataFrame(readTableGiocatori(), colums = ["Ruolo", "Cognome", "Squadra", "Media", "Quotazione"])
 
 #Apllicazione
 st.dataframe(table)
