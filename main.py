ccb0f9f5326ae6f63a55a876a8dff7e05b89822b
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-14 12:10:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ st.dataframe(listaGiocatori)
 st.write(list(client["Fantacalcio"]["listaChiamati"].find())[0]["lista"])
 st.title("Applicazione Fanta")
 if st.button('Estrai Giocatore'):
-    giocatore = session.listaGiocatori.sample(n=1)
-    session.listaGiocatori = session.listaGiocatori.drop(giocatore.index, axis = 0, inplace = True)
+    giocatore = listaGiocatori.sample(n=1)
+    session.listaGiocatori = listaGiocatori.drop(giocatore.index, axis = 0, inplace = True)
     session.ruolo = giocatore["ruolo"].values[0]
     session.cognome = giocatore["cognome"].values[0]
     session.squadra = giocatore["squadra"].values[0]
