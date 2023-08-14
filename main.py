77bc6b9ea79d63651af49d2ee34184103dcf5ed2
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-08-14 11:43:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ st.dataframe(session.listaGiocatori)
 st.title("Applicazione Fanta")
 if st.button('Estrai Giocatore'):
     giocatore = session.listaGiocatori.sample(n=1)
-    session.listaGiocatori.drop(giocatore.index, axis = 0, inplace = True)
+    session.listaGiocatori = session.listaGiocatori.drop(giocatore.index, axis = 0, inplace = True)
     session.listaGiocatori.reset_index(inplace=True)
     session.ruolo = giocatore["ruolo"].values[0]
     session.cognome = giocatore["cognome"].values[0]
