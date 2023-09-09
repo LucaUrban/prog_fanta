5988834cea0f336d89e565786e372756c1565f18
LucaUrban <56407271+LucaUrban@users.noreply.github.com>
2023-09-09 11:43:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ def inserisciGiocatoreDataframe(df, partecipante, ruolo, cognome, price):
     collection.find_one_and_update({"Partecipante": partecipante}, {"$set": {"Squadra": loads(df.to_json(orient="records"))}})
 
 #crea la tabella riassuntiva delle spese per un singolo partecipante
-def creaTabRiassuntivaSpese(df):
+def creaTabRiassuntivaSpese(df, crediti_rimanenti):
     return pd.DataFrame([sum(df[df["Ruolo"].str.contains("P")]["Prezzo"].values), sum(df[df["Ruolo"].str.contains("D")]["Prezzo"].values), sum(df[df["Ruolo"].str.contains("C")]["Prezzo"].values),
-                         sum(df[df["Ruolo"].str.contains("A")]["Prezzo"].values), 500 - sum(df["Prezzo"].values), 501 - sum(df["Prezzo"].values) - len(df[df["Prezzo"] == 0]["Prezzo"].values)],
+                         sum(df[df["Ruolo"].str.contains("A")]["Prezzo"].values), crediti_rimanenti - sum(df["Prezzo"].values), (crediti_rimanenti+1) - sum(df["Prezzo"].values) - len(df[df["Prezzo"] == 0]["Prezzo"].values)],
                          columns = ["Crediti"], index = ["Cr. Portieri", "Cr. Difensori", "Cr. Centrocampisti", "Cr. Attaccanti", "Cr. Rimanenti", "Cr. Max Spendibili"])
 
 # Connection to the cluster
@@ -39,8 +39,10 @@ def get_data():
     return items
 
 data = get_data()
-listaChiamati = loads(list(client["Fantacalcio"]["listaChiamati"].find())[0]["lista"])
 session = st.session_state
+
+#creazione dizionario crediti rimanenti
+dict_crediti_rim = {item["Partecipante"]: item["crediti_rimanenti"] for item in data}
 
 #costruzione dataframe giocatori
 Alessandro = pd.DataFrame(next(item for item in data if item["Partecipante"] == "Alessandro")["Squadra"], columns = ["Ruolo", "Cognome", "Prezzo"])
@@ -54,7 +56,6 @@ Valter = pd.DataFrame(next(item for item in data if item["Partecipante"] == "Val
 
 #importazione lista calciatori
 listaGiocatori = pd.read_csv('https://raw.githubusercontent.com/LucaUrban/prog_fanta/main/fanta/ListaGiocatori.CSV', delimiter = ";")
-listaGiocatori.drop(listaChiamati, inplace = True)
 
 #Apllicazione
 st.title("Applicazione Fanta")
@@ -89,40 +90,40 @@ with col1:
     st.write("Alessandro")
     st.dataframe(Alessandro, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Alessandro))
+    st.table(creaTabRiassuntivaSpese(Alessandro, dict_crediti_rim["Alessandro"]))
     st.write("")
     st.write("Luca")
     st.dataframe(Luca, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Luca))
+    st.table(creaTabRiassuntivaSpese(Luca, dict_crediti_rim["Luca"]))
 with col2:
     st.write("Andrea")
     st.dataframe(Andrea, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Andrea))
+    st.table(creaTabRiassuntivaSpese(Andrea, dict_crediti_rim["Andrea"]))
     st.write("")
     st.write("Michele")
     st.dataframe(Michele, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Michele))
+    st.table(creaTabRiassuntivaSpese(Michele, dict_crediti_rim["Michele"]))
 with col3:
     st.write("Federico")
     st.dataframe(Federico, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Federico))
+    st.table(creaTabRiassuntivaSpese(Federico, dict_crediti_rim["Federico"]))
     st.write("")
     st.write("Valerio")
     st.dataframe(Valerio, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Valerio))
+    st.table(creaTabRiassuntivaSpese(Valerio, dict_crediti_rim["Valerio"]))
 with col4:
     st.write("Gabriele")
     st.dataframe(Gabriele, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Gabriele))
+    st.table(creaTabRiassuntivaSpese(Gabriele, dict_crediti_rim["Gabriele"]))
     st.write("")
     st.write("Valter")
     st.dataframe(Valter, use_container_width = True, height = 915, column_config={"Ruolo": st.column_config.ImageColumn("Ruolo")}, hide_index = True)
     st.write("")
-    st.table(creaTabRiassuntivaSpese(Valter))
+    st.table(creaTabRiassuntivaSpese(Valter, dict_crediti_rim["Valter"]))
 
